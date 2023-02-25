505d6a7802ed99d12c4e98b9ab31608e2b88ec1d
Egor Podolskiy <eapodolskiy@avito.ru>
2023-02-25 11:24:22 +0300

    add delete counter from MainPage for passing CI ui tests

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Counter } from 'entities/Counter';
 
 function MainPage() {
     const { t } = useTranslation('main');
@@ -8,7 +7,6 @@ function MainPage() {
     return (
         <div>
             {t('Главная страница')}
-            <Counter />
         </div>
     );
 }
