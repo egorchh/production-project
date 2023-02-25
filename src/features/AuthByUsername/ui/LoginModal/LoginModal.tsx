79ddebf9b59efcd55b70e4712ac508f01183a8a5
Egor Podolskiy <eapodolskiy@avito.ru>
2023-02-25 22:07:45 +0300

    add pre-commit hooks (husky), apply changed screenshots

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from 'features/AuthByUsername/ui/LoginForm/LoginForm';
-import styles from './LoginModal.module.scss';
 
 interface LoginModalProps {
     className?: string;
@@ -14,7 +13,7 @@ export const LoginModal = (props: LoginModalProps) => {
 
     return (
         <Modal
-            className={classNames(styles.loginModal, {}, [className])}
+            className={classNames('', {}, [className])}
             isOpen={isOpen}
             onClose={onClose}
         >
