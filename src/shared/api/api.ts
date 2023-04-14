0642be95477158eec4d893e88e2c09fa0c019b46
Egor Podolskiy <eapodolskiy@avito.ru>
2023-04-14 10:03:01 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
-import { LOCAL_STORAGE_THEME_KEY } from 'app/providers/ThemeProvider/lib/ThemeContext';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+
+    return config;
 });
