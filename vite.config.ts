d68335ec6173bcfc1cb55c1511ae8b4d2bee2f4a
Egor Podolskiy <eapodolskiy@avito.ru>
2023-05-14 19:32:20 +0300

    add vite for development (not good practice, only for study)

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+    plugins: [
+        svgr({ exportAsDefault: true }),
+        react(),
+    ],
+    resolve: {
+        alias: [
+            { find: '@', replacement: '/src' },
+        ],
+    },
+    define: {
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify('http://localhost:8000'),
+        __PROJECT__: JSON.stringify('frontend'),
+    },
+});
