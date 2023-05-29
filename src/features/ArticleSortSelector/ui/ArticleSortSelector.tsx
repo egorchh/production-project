34c2f4cc4a928d22f71ff34314bd6f4ad34bd2ee
Egor Podolskiy <eapodolskiy@avito.ru>
2023-05-29 10:48:01 +0300

    fix some errors

--- src/features/ArticleSortSelector/ui/ArticleSortSelector.tsx
+++ src/features/ArticleSortSelector/ui/ArticleSortSelector.tsx
@@ -2,7 +2,7 @@ import { memo, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Select, SelectOption } from '@/shared/ui/Select';
-import { SortOrder } from '@/shared/types';
+import { SortOrder } from '@/shared/types/sort';
 import { ArticleSortField } from '@/entities/Article';
 import styles from './ArticleSortSelector.module.scss';
 
