d5c1ea02be91f071c7078cd9fb2075490d0f36a4
Egor Podolskiy <eapodolskiy@avito.ru>
2023-06-05 09:26:20 +0300

    add types for buildSelector

--- src/shared/store/buildSelector.ts
+++ src/shared/store/buildSelector.ts
@@ -1,11 +1,12 @@
 import { useSelector } from 'react-redux';
 import { StateSchema } from '@/app/providers/StoreProvider';
 
-type Selector<T> = (state: StateSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+type Selector<T, Args extends any[]> = (state: StateSchema, ...args: Args) => T;
+type Hook<T, Args extends any[]> = (...args: Args) => T;
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>];
 
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
-    const useSelectorHook = () => useSelector(selector);
+export function buildSelector<T, Args extends any[]>(selector: Selector<T, Args>): Result<T, Args> {
+    const useSelectorHook: Hook<T, Args> = (...args: Args) => useSelector((state: StateSchema) => selector(state, ...args));
 
     return [useSelectorHook, selector];
 }
